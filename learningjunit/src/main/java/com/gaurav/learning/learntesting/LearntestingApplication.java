d792260581448a5dc5547aaff6444c32a79a0a0a
nikumbgo <prtpgrv@yahoo.com>
2019-03-08 19:59:32 +0530

    first commit
    first commit

---
+++ learningjunit/src/main/java/com/gaurav/learning/learntesting/LearntestingApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.learning.learntesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearntestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearntestingApplication.class, args);
+	}
+
+}
---
+++ learningjunit/src/test/java/com/gaurav/learning/learntesting/LearntestingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gaurav.learning.learntesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearntestingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
